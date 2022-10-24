00fa347ab9b75ac5287b74c60d7c0b48eef968b8
tranvanhieu2002 <hieu.tran23@student.passerellesnumeriques.org>
2022-10-24 18:29:36 +0700

    add utf8mb4

--- wp-config.php
+++ wp-config.php
@@ -32,7 +32,7 @@ define( 'DB_PASSWORD', '' );
 define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf16' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
