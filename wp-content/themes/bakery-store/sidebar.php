1f53c4eccb22d125048014b6eef07a677720cdcc
hieu tran <hieu.tran23@student.passerellesnumeriques.org>
2022-10-27 00:22:04 -0400

    remove some unneccerry themes

--- wp-content/themes/bakery-store/sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! is_active_sidebar( 'bakerystore-sidebar-primary' ) ) {	return; } ?>
-<div class="col-lg-4">
-	<div class="sidebar">
-		<?php dynamic_sidebar('bakerystore-sidebar-primary'); ?>
-	</div>
-</div>
--- wp-content/themes/twentytwentytwo-child/services/RestApi/Model/products.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function get_products(){
-    global $wpdb;
-    $query = "SELECT * FROM wp_t_products";
-    return  $wpdb->query($query);
-}
-
-?>
